52dc37337cdadb2a43a05eba74d19bd2d0919238
Xushnudd <boshhacker1@gmail.com>
2025-05-22 14:52:41 +0500

    asdf

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -3,7 +3,3 @@
 # Create a variable 'num_two'   and assign it the value of the int 4.
 
 # Create a variable 'variable_total' and assign it the sum of  'num_one' and 'num_two'.
-
-# sdf
-# sdf
-# sdf
